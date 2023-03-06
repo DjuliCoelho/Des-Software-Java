bc90cbb4a52766ac404e6e16c8b0b8ba982fa2ef
Djulinda <126629987+DjuliCoelho@users.noreply.github.com>
2023-03-06 11:29:06 -0300

    Add files via upload

---
+++ repeticao.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class repeticao {
+public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		//String[] mat = new String [5]; 
+		//String[] nome = new String [5];
+		//int[] nota1 = new int [5] ;
+		//int[] nota2 = new int [5];
+		
+		//for(int i = 0;i < 5;i++){
+		    //System.out.println("Digite a matricula:");
+		   //mat[i] = leitor.nextLine();
+		
+		//System.out.println("Informe o nome:");
+		//nome[i] = leitor.nextLine();
+		
+		//System.out.println("Informe nota 1");
+		//nota1[i] = leitor.nextInt();
+		
+		//System.out.println("Informe nota 2");
+		//nota2[i] = leitor.nextInt();
+		
+		//leitor.nextLine(); Força quebra de linha
+//}
+		
+		//int contador = 0;
+		//int qtde = 5;
+		//while (cobtador < qtde) {
+		
+		//System.out.println("Matricula:" + mat[contador]);
+		//System.out.println("Nome:" + nome[contador]);
+		
+		//int notaF = (nota1[contador] + nota2[contador) / 2;
+		
+		//if (notaF >=6){
+		//System.out.println("Aprovado ( x ) Reprovado ( );
+		//System.out.println("Aprovado (  ) Reprovado ( x );
+		// contador ++;
+		
+		
+		
+		String mat; //declara vetor aqui
+		String nome;
+		int nota1;
+		int nota2;
+		System.out.println("Num matricula");
+		mat = leitor.nextLine();
+		
+		System.out.println("Nome do aluno");
+		nome = leitor.nextLine();
+		
+		System.out.println("Nota 1");
+		nota1 = leitor.nextInt();
+		
+		System.out.println("Nota 2");
+		nota2 = leitor.nextInt();
+		
+		int nf = (nota1 + nota2);
+		
+		
+		
+		
+}
+
+}
